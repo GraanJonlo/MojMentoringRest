c63e673b5eb96fd35d1aad827249adf15574ded7
Andrew Grant <andy.a.grant@gmail.com>
2017-10-02 11:12:38 +0100

    More validation and canonicalization

--- src/Api/Controllers/CategoriesController.cs
+++ src/Api/Controllers/CategoriesController.cs
@@ -7,27 +7,27 @@ namespace Api.Controllers
     [Route("api/v1/[controller]")]
     public class CategoriesController : Controller
     {
-        [HttpPost]
-        public IActionResult Post([FromBody] DomainTypes.CreateCategory dto)
-        {
-            var category = UseCases.createCategory(dto);
+         [HttpPost]
+         public IActionResult Post([FromBody] DomainTypes.CreateCategoryCommand dto)
+         {
+             var category = UseCases.createCategory(dto);
 
-            if (category.IsOk)
-            {
-                var result = new CategoryDto
-                {
-                    Id = category.ResultValue.id,
-                    Name = category.ResultValue.name,
-                    Slug = category.ResultValue.slug,
-                    Description = category.ResultValue.description,
-                    Status = category.ResultValue.status
-                };
-                return Created("/api/v1/categories/" + result.Id, result);
-            }
-            else
-            {
-                return BadRequest(category.ErrorValue);
-            }
-        }
+             if (category.IsOk)
+             {
+                 var result = new CategoryDto
+                 {
+                     Id = category.ResultValue.id,
+                     Name = category.ResultValue.name,
+                     Slug = category.ResultValue.slug,
+                     Description = category.ResultValue.description,
+                     Status = category.ResultValue.status
+                 };
+                 return Created("/api/v1/categories/" + result.Id, result);
+             }
+             else
+             {
+                 return BadRequest(category.ErrorValue);
+             }
+         }
     }
 }
